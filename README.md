f66c232b16d62e9e378d610bd4704322d8cac129
jeremybuis <buis.jeremy@gmail.com>
2018-11-08 15:34:23 -0500

    adds linting to project

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# eslint-plugin-ss
+# eslint-plugin-ssrules
 
 A collection of security rules for JavaScript
 
@@ -10,22 +10,22 @@ You'll first need to install [ESLint](http://eslint.org):
 $ npm i eslint --save-dev
 ```
 
-Next, install `eslint-plugin-ss`:
+Next, install `eslint-plugin-ssrules`:
 
 ```
-$ npm install eslint-plugin-ss --save-dev
+$ npm install eslint-plugin-ssrules --save-dev
 ```
 
 **Note:** If you installed ESLint globally (using the `-g` flag) then you must also install `eslint-plugin-ss` globally.
 
 ## Usage
 
-Add `ss` to the plugins section of your `.eslintrc` configuration file. You can omit the `eslint-plugin-` prefix:
+Add `ssrules` to the plugins section of your `.eslintrc` configuration file. You can omit the `eslint-plugin-` prefix:
 
 ```json
 {
     "plugins": [
-        "ss"
+        "ssrule"
     ]
 }
 ```
@@ -36,14 +36,14 @@ Then configure the rules you want to use under the rules section.
 ```json
 {
     "rules": {
-        "ss/rule-name": 2
+        "ssrules/bad-hash-algorithm": 2
     }
 }
 ```
 
 ## Supported Rules
 
-* Fill in provided rules here
+- ssrules/bad-hash-algorithm - Checks for use of md5 and sha1
 
 
 
--- tests/lib/rules/bad-hash-algorithm.js
+++ tests/lib/rules/bad-hash-algorithm.js
@@ -2,57 +2,56 @@
  * @fileoverview Checks for the use of bad hashing algorithms that have been shown to collide
  * @author Jeremy Buis
  */
-"use strict";
+'use strict';
 
-//------------------------------------------------------------------------------
+// ------------------------------------------------------------------------------
 // Requirements
-//------------------------------------------------------------------------------
+// ------------------------------------------------------------------------------
 
-var rule = require("../../../lib/rules/bad-hash-algorithm"),
+var rule = require('../../../lib/rules/bad-hash-algorithm');
 
-    RuleTester = require("eslint").RuleTester;
+var RuleTester = require('eslint').RuleTester;
 
-
-//------------------------------------------------------------------------------
+// ------------------------------------------------------------------------------
 // Tests
-//------------------------------------------------------------------------------
+// ------------------------------------------------------------------------------
 
 var ruleTester = new RuleTester();
-ruleTester.run("bad-hash-algorithm", rule, {
+ruleTester.run('bad-hash-algorithm', rule, {
 
     valid: [
 
         // give me some code that won't trigger a warning
         {
-            code: "crypto.createHash('sha512').update(data).digest('hex')",
+            code: 'crypto.createHash(\'sha512\').update(data).digest(\'hex\')',
             errors: [{
-                message: "Fill me in.",
-                type: "Me too"
-            }],
+                message: 'Fill me in.',
+                type: 'Me too'
+            }]
         },
 
         {
-            code: "crypto.createHash('sha256').update(data).digest('hex')",
+            code: 'crypto.createHash(\'sha256\').update(data).digest(\'hex\')',
             errors: [{
-                message: "Fill me in.",
-                type: "Me too"
+                message: 'Fill me in.',
+                type: 'Me too'
             }]
         }
     ],
 
     invalid: [
         {
-            code: "crypto.createHash('md5').update(data).digest('hex')",
+            code: 'crypto.createHash(\'md5\').update(data).digest(\'hex\')',
             errors: [{
-                message: "MD5 is a weak hash algorithm with collisions.",
-                type: "CallExpression"
+                message: 'MD5 is a weak hash algorithm with collisions.',
+                type: 'CallExpression'
             }]
         },
         {
-            code: "crypto.createHash('sha1').update(data).digest('hex')",
+            code: 'crypto.createHash(\'sha1\').update(data).digest(\'hex\')',
             errors: [{
-                message: "SHA1 is a weak hash algorithm with collisions.",
-                type: "CallExpression"
+                message: 'SHA1 is a weak hash algorithm with collisions.',
+                type: 'CallExpression'
             }]
         }
     ]
